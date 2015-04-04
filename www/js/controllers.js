89f1b0d27f08f9d3617308e25457b54a5b0e1edd
Bharat Arimilli <bbarimilli@outlook.com>
2015-04-04 18:30:10 -0400

    Initial Commit.

---
+++ www/js/controllers.js
@@ -0,0 +1,151 @@
+angular.module('ExonianMobile.controllers', ['ionic'])
+
+.controller('AppController', function($scope, MenuFactory) {
+    $scope.MenuFactory = MenuFactory;
+    ionic.Platform.fullScreen();
+})
+
+.controller('HeadlinesController', function($scope, PostsFactory, $ionicLoading, $state, UtilityFactory) {
+    $ionicLoading.show({
+        template: "Loading"
+    });
+    $scope.Page = 1;
+    PostsFactory.getRecentPosts().success(function(response) {
+        $scope.Posts = UtilityFactory.processAuthors(response.posts);
+        $scope.MaxPage = response.pages;
+        $ionicLoading.hide();
+    });
+    PostsFactory.getFeaturedPost().success(function(response) {
+        $scope.Featured = response.posts[0];
+        if($scope.Featured.attachments[0].url) {
+            $('.nav-bar-large').css('background-image', 'url(' +       $scope.Featured.attachments[0].url + ')');            
+        }
+    });
+    $scope.goToHeadline = function(headline) {
+        $state.go('app.article', { articleId:headline.id });
+    }
+    $scope.checkData = function() {
+        var pages = $scope.Page + 1;
+        if(pages > $scope.MaxPage) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    $scope.loadMore = function() {
+        $scope.Page += 1;
+        PostsFactory.getRecentPage($scope.Page).success(function(response) {
+            var posts = UtilityFactory.processAuthors(response.posts);
+            for(var i = 0; i < posts.length; i++) {
+                if($scope.Posts) {
+                    $scope.Posts.push(posts[i]);
+                }
+            }
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    }
+})
+
+.controller('SearchController', function($scope, $ionicLoading, SearchFactory) {
+    $scope.search = function(searchTerm) {
+        $ionicLoading.show({
+            template: "Loading"
+        });
+        $scope.Page = 1;
+        SearchFactory.searchPosts(searchTerm).success(function(response) {
+            $scope.Posts = response.posts;
+            $scope.CurrentTerm = searchTerm;
+            $scope.MaxPage = response.pages;
+            $ionicLoading.hide();
+        });
+    }
+    $scope.checkData = function() {
+        var pages = $scope.Page + 1;
+        if($scope.MaxPage) {
+            if(pages > $scope.MaxPage) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    $scope.loadMore = function() {
+        $scope.Page += 1;
+        SearchFactory.searchPostsPage($scope.CurrentTerm, $scope.Page).success(function(response) {
+            var posts = response.posts;
+            for(var i = 0; i < posts.length; i++) {
+                if($scope.Posts) {
+                    $scope.Posts.push(posts[i]);
+                }
+            }
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    }
+})
+
+.controller('CategoryController', function($scope, PostsFactory, UtilityFactory, $ionicLoading, $stateParams) {
+    $scope.Page = 1;
+    PostsFactory.getCategory($stateParams.categoryName).success(function(response) {
+        $scope.Posts = UtilityFactory.processAuthors(response.posts);
+        $scope.MaxPage = response.pages;
+        $scope.Title = UtilityFactory.findCategory($stateParams.categoryName);
+    });
+    $scope.checkData = function() {
+        var pages = $scope.Page + 1;
+        if(pages > $scope.MaxPage) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    $scope.loadMore = function() {
+        $scope.Page += 1;
+        PostsFactory.getCategoryPage($stateParams.categoryName, $scope.Page).success(function(response) {
+            var posts = UtilityFactory.processAuthors(response.posts);
+            for(var i = 0; i < posts.length; i++) {
+                if($scope.Posts) {
+                    $scope.Posts.push(posts[i]);
+                }
+            }
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    }
+})
+
+.controller('ArticleController', function($scope, PostsFactory, $ionicLoading, $stateParams) {
+    $ionicLoading.show({
+        template: "Loading"
+    });
+    PostsFactory.getPost($stateParams.articleId).success(function(response){
+        $scope.Post = response.post;
+        if($scope.Post.custom_fields.authors[0]) {
+            if($scope.Post.custom_fields.authors[0] === "") {
+                $scope.Author = $scope.Post.author.name;
+            }
+            else {
+                $scope.Author = $scope.Post.custom_fields.authors[0];
+            }
+        }
+        else {
+            $scope.Author = $scope.Post.author.name;
+        }
+        if($scope.Post.attachments.length > 0) {
+            $scope.Image = $scope.Post.attachments[0].url;
+            $scope.ImageString = "background-image: url(" + $scope.Image + "); ";
+            var img = new Image();
+            img.addEventListener("load", function() {
+                if(img.width > img.height) {
+                    $('.article-image').addClass('article-image-wide');
+                }
+            }, false);
+            img.src = $scope.Image;
+        }
+        $ionicLoading.hide();
+    });
+});
---
+++ www/js/services.js
@@ -0,0 +1,85 @@
+angular.module('ExonianMobile.services', [])
+
+.factory('MenuFactory', function() {
+    return [
+        { id:'news', name:'News' },
+        { id:'exeter-life', name:'Life' },
+        { id:'opinion', name:'Opinion' },
+        { id:'humor', name:'Humor' },
+        { id:'sports', name:'Sports' }
+    ];
+})
+
+.factory('PostsFactory', function($http) {
+    return {
+        getRecentPosts: function() {
+            return $http.get('http://www.theexonian.com/?json=get_recent_posts');
+        },
+        getRecentPage: function(page) {
+            return $http.get('http://www.theexonian.com/?json=get_recent_posts&page=' + page);
+        },
+        getFeaturedPost: function() {
+            return $http.get('http://www.theexonian.com/api/core/get_category_posts/?category_slug=featured');
+        },
+        getPost: function(id) {
+            return $http.get('http://www.theexonian.com/api/core/get_post/?id=' + id);
+        },
+        getCategory: function(categoryName) {
+            return $http.get('http://www.theexonian.com/api/core/get_category_posts/?category_slug=' + categoryName);
+        },
+        getCategoryPage: function(categoryName, page) {
+            return $http.get('http://www.theexonian.com/api/core/get_category_posts/?category_slug=' + categoryName + "&page=" + page);
+        }
+    }
+})
+
+.factory('SearchFactory', function($http) {
+    return {
+        searchPosts: function(searchTerm) {
+            return $http.get('http://www.theexonian.com/api/core/get_search_results/?search=' + searchTerm);
+        },
+        searchPostsPage: function(searchTerm, page) {
+            return $http.get('http://www.theexonian.com/api/core/get_search_results/?search=' + searchTerm + "&page=" + page);
+        }
+    }
+})
+
+.factory('UtilityFactory', function(MenuFactory) {
+    return {
+        stringifyArray: function(array) {
+            var string = "";
+            for(var i = 0; i < array.length; i++) {
+                if(i === 0) {
+                    string += array[i];
+                }
+                else {
+                    string += ", " + array[i];
+                }
+            }
+        },
+        processAuthors: function(posts) {
+            for(var i = 0; i < posts.length; i++) {
+                if(posts[i].custom_fields.authors[0]) {
+                    if(posts[i].custom_fields.authors[0] === "") {
+                        posts[i].Author = posts[i].author.name;
+                    }
+                    else {
+                        posts[i].Author = posts[i].custom_fields.authors[0];
+                    }
+                }
+                else {
+                    posts[i].Author = posts[i].author.name;
+                }
+            }
+            return posts;
+        },
+        findCategory: function(categoryName) {
+            var menu = MenuFactory;
+            for(var i = 0; i < menu.length; i++) {
+                if(categoryName === menu[i].id) {
+                    return menu[i].name;
+                }
+            }
+        }
+    }
+});
